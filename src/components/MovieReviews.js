294044a9aa1c5535b706c8b278c7d086163ed185
Derek Chen <derekgc@icloud.com>
2018-07-02 16:19:28 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,19 @@
 // Code MovieReviews Here
+import React, { Component } from 'react';
+
+const MovieReviews = (props) => {
+    const reviewsGeneated = props.reviews.map(review => <li className="review" >{review.display_title}</li>)
+    return (
+        <div className= "review-list">
+            <ul>
+            {reviewsGeneated}
+            </ul>
+        </div>
+    )
+}
+
+MovieReviews.defaultProps = {
+    reviews: []
+}
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,29 @@
 // Code SearchableMovieReviewsContainer Here
+
+import React, { Component } from 'react';
+
+class SearchableMovieReviewsContainer extends Component {
+    state={
+        reviews: [],
+        searchTerm: ""
+    }
+
+    handleChange= (e)=>{
+        this.setState({searchTerm: e.target.value})
+    }
+    render() {
+        let filter = this.state.reviews;
+        if(this.state.searchTerm){
+            filter = this.state.reviews.filter(r => r.summary_short.includes(this.state.searchTerm))
+        }
+        let reviews = filter.map(review => <li className="review" >{review.display_title}</li>)
+        return (
+            <div className ="searchable-movie-reviews">
+                <input type="text" value={this.state.searchTerm} onChange = {this.handleChange} />
+                {reviews}
+            </div>
+        );
+    }
+}
+
+export default SearchableMovieReviewsContainer;
